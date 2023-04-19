9fab27281e653dc58713e05ae41b5101bc7fe6a1
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-04-19 13:53:41 +0200

    Add files via upload

---
+++ DAY 1/Activity 3/example1.js
@@ -0,0 +1,4 @@
+function basicFunction () {  //the name of the function is basicFunction
+    alert ("Hello JavaScripters!");  //an alert box is dislayed when the page loads
+}
+basicFunction(); //calling the function
---
+++ DAY 1/Activity 3/example2.js
@@ -0,0 +1,7 @@
+var Ret = lefty ("This is a test", 7);
+alert (Ret);
+function lefty (InString, Num) {
+    var OutString=InString.substring (InString, Num);
+    return (OutString);
+}
+lefty("This is a test", 7);
