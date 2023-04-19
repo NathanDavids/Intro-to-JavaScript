9a2a440a895d5f3204876211930607e5ef04eca7
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-04-19 13:55:01 +0200

    Add files via upload

---
+++ WEEK 3/DAY 1/ACTIVITY 2/onSubmit.js
@@ -0,0 +1,9 @@
+function ValidationEvent() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").valid;
+    var contact = document.getElementById("contact").valid;
+    var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+
+    if (name != '' && email != '' && contact != '') {
+        if (email)
+    }
